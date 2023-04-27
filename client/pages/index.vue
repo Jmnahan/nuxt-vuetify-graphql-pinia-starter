0db72bde1555aaff7945e5a9d7cfca1b9dd1a63a
Jhona Rica Manahan <manhan.jrd@gmail.com>
2023-04-28 00:04:38 +0800

    index-page-style

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,120 +1,95 @@
 <template>
 	<v-container>
-		<h2>
-			<v-icon icon="mdi-vuetify" />
-			Starter Template
+		<h2 class="text-center text-lg-h4 font-weight-bold my-2">
+			<v-icon icon="mdi-rocket-launch" />
+			There are {{ launches?.length || 0 }} Missions!
 		</h2>
-		<h5>Nuxt 3 / Vuetify / Graphql / Pinia</h5>
-		<h3 class="my-5">
-			Example Pinia
-			<v-chip color="blue">useCounter</v-chip>
-		</h3>
-		<v-card class="mx-auto my-12" max-width="374">
-			<v-card-title class="text-blue">Pinia useCounter()</v-card-title>
-			<v-card-item>
-				<v-card-text>
-					<v-chip>count:</v-chip>
-					{{ store.count }}
-				</v-card-text>
-				<v-card-text>
-					<v-chip>doubleCount:</v-chip>
-					{{ store.doubleCount }}
-				</v-card-text>
-			</v-card-item>
-
-			<v-card-actions><v-btn color="blue" @click="store.increment()">Increment</v-btn></v-card-actions>
-		</v-card>
-
-		<h3 class="my-5">
-			Example Vuetify
-			<v-chip color="blue">Card</v-chip>
-		</h3>
-		<v-card class="mx-auto my-12" max-width="374">
-			<template #progress>
-				<v-progress-linear color="deep-purple" height="10" indeterminate />
-			</template>
-
-			<v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png" />
-
-			<v-card-title>Cafe Badilico</v-card-title>
-
-			<v-card-text>
-				<v-row align="center" class="mx-0">
-					<v-rating :value="4.5" color="amber" dense half-increments readonly size="14" />
-
-					<div class="grey--text ms-4">4.5 (413)</div>
-				</v-row>
-
-				<div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
-
-				<div>
-					Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio
-					seating.
-				</div>
-			</v-card-text>
-
-			<v-divider class="mx-4" />
-
-			<v-card-title>Tonight's availability</v-card-title>
-
-			<v-card-text>
-				<v-chip-group v-model="selection" active-class="deep-purple accent-4 white--text" column>
-					<v-chip>5:30PM</v-chip>
-
-					<v-chip>7:30PM</v-chip>
-
-					<v-chip>8:00PM</v-chip>
-
-					<v-chip>9:00PM</v-chip>
-				</v-chip-group>
-			</v-card-text>
-
-			<v-card-actions>
-				<v-btn color="deep-purple lighten-2">Reserve</v-btn>
-			</v-card-actions>
-		</v-card>
-		<h3 class="my-5">
-			Example Vuetify
-			<v-chip color="blue">SimpleTable</v-chip>
-			<v-chip color="orange">Data from spaceX graphql</v-chip>
-		</h3>
-		<p>There are {{ ships?.length || 0 }} ships.</p>
-		<v-table>
-			<thead>
-				<tr>
-					<th class="text-left">Name</th>
-					<th class="text-left">Active</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr v-for="ship in ships" :key="ship.name">
-					<td>{{ ship.name }}</td>
-					<td>
-						<v-chip :color="ship.active ? 'green' : 'red'">{{ ship.active }}</v-chip>
-					</td>
-				</tr>
-			</tbody>
-		</v-table>
+		<v-container>
+			<v-row class="align-center justify-center mb-4">
+				<label class="text-lg-h6 px-2" for="year-input">Filter by year:</label>
+				<input
+					id="year-input"
+					v-model="year"
+					class="year-input text-lg-h6 px-4 py-2 rounded"
+					type="number"
+					:min="minYear"
+					:max="maxYear"
+				/>
+				<label class="text-lg-h6 ms-lg-16 px-2" for="order-select">Order:</label>
+				<select
+					id="order-select"
+					v-model="ascending"
+					class="order-select text-lg-h6 px-4 py-2 rounded"
+				>
+					<option :value="true">Ascending</option>
+					<option :value="false">Descending</option>
+				</select>
+			</v-row>
+		</v-container>
+		<div v-for="launch in sortedAndFilteredLaunches" :key="launch.id">
+			<LaunchCard :launch="launch" />
+		</div>
 	</v-container>
 </template>
 <script lang="ts" setup>
-const store = useCounter()
-const selection = ref(0)
+import useSortAndFilterLaunches from '../composables/SortLaunches'
+
+interface Launch {
+	id: string
+	mission_name: string
+	launch_date_local: string
+	details: string
+	rocket: {
+		rocket_name: string
+		rocket: {
+			id: string
+		}
+	}
+	launch_site: {
+		site_name: string
+	}
+}
+
 const query = gql`
-	query getShips {
-		ships {
+	query getLaunches {
+		launches {
 			id
-			name
-			active
+			mission_name
+			launch_date_local
+			details
+			rocket {
+				rocket_name
+				rocket {
+					id
+				}
+			}
+			launch_site {
+				site_name
+			}
 		}
 	}
 `
-const { data } = useAsyncQuery<{
-	ships: {
-		id: String
-		name: String
-		active: Boolean
-	}
-}>(query)
-const ships = computed(() => data.value?.ships ?? [])
+const { data } = await useAsyncQuery<{ launches: Launch[] }>(query)
+const launches = computed(() => data.value?.launches ?? [])
+const year = ref(2021)
+const ascending = ref(true)
+const { sortedAndFilteredLaunches } = useSortAndFilterLaunches(data.value?.launches ?? [], year, ascending)
+
+const minYear = computed(() =>
+	Math.min(...launches.value.map((launch) => new Date(launch.launch_date_local).getFullYear())),
+)
+
+const maxYear = computed(() =>
+	Math.max(...launches.value.map((launch) => new Date(launch.launch_date_local).getFullYear())),
+)
 </script>
+
+<style>
+.year-input {
+	border: 1px solid #ccc;
+}
+
+.order-select {
+	border: 1px solid #ccc;
+}
+</style>
